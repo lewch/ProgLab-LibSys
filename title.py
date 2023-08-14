35666b18792490cddbe7c84a962da1e450ad3867
Leo <32805428+lewch@users.noreply.github.com>
2023-08-14 17:14:19 +0100

    Created a title class

---
+++ title.py
@@ -0,0 +1,15 @@
+class Title:
+    '''
+    A class for titles.
+    '''
+    def __init__(self, name, publisher, genre, quantity):
+        '''
+        :param name: names of the books
+        :param publisher: publisher of the books
+        :param genre: genre of the books
+        :param quantity: number of the books
+        '''
+        self.name = name
+        self.publisher = publisher
+        self.genre = genre
+        self.quantity = quantity
