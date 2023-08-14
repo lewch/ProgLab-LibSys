32ce5924c0051004d3fa3709a80223036853e918
Leo <32805428+lewch@users.noreply.github.com>
2023-08-14 18:20:29 +0100

    Added getters and setters

--- title.py
+++ title.py
@@ -13,3 +13,39 @@ class Title:
         self.publisher = publisher
         self.genre = genre
         self.quantity = quantity
+
+    @property
+    def quantity(self):
+        return self.quantity
+
+    @quantity.setter
+    def quantity(self, value):
+        self.quantity = value
+
+    @property
+    def name(self):
+        return self.name
+
+    @name.setter
+    def name(self, value):
+        self.name = value
+
+    @property
+    def genre(self):
+        return self.genre
+
+    @genre.setter
+    def genre(self, value):
+        self.genre = value
+
+    @property
+    def publisher(self):
+        return self.publisher
+
+    @publisher.setter
+    def publisher(self, value):
+        self.publisher = value
+
+
+
+
