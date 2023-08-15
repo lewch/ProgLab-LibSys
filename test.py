2492b8c8ff5c37a676c669e842b03f9234d64ba7
Leo <32805428+lewch@users.noreply.github.com>
2023-08-15 22:11:21 +0100

    Created an remove_title method to remove titles in the database

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
 from title_manager import *
-
+import copy
 # Connect to the database called 'library.db'
-tm = title_manager("library.db")
+# tm = title_manager("library.db")
+tm = title_manager(":memory:")
 # Sample titles
 t1 = Title("Hello Python", "BirFlex Inc.", "Mining", 5)
 t2 = Title("Good Morning", "Alex Inc.", "Biology", 7)
@@ -16,6 +17,9 @@ tm.show()
 # Amend a title
 tm.update_title("Good Morning", new_publisher="Bill Inc.")
 tm.update_title("Hello Python", new_genre= "Psychology", new_publisher="Bill Inc.")
+
+# Remove a title
+tm.remove_title("Good Morning")
 
 # Show all the titles
 print()
