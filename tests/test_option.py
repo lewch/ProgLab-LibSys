a624a2447399738f7a083d4bc9e20b80b8775f7d
Leo <32805428+lewch@users.noreply.github.com>
2023-08-18 20:30:13 +0100

    Build tests

---
+++ tests/test_option.py
@@ -0,0 +1,11 @@
+import pytest
+class TestOption:
+    def test_execute(self):
+        assert False
+    def test_one(self):
+        x = "this"
+        assert "h" in x
+
+    def test_two(self):
+        x = "hello"
+        assert hasattr(x, "check")
